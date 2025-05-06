8a98258d34cec252cdd662e66d48cc48777e5bea
Emily_Nohelia <emilysanchez7193@gmail.com>
2025-05-05 22:28:50 -0600

    Subiendo cambios en el ejercicio 4

--- Ejercicio#4.py
+++ Ejercicio#4.py
@@ -1,31 +1,34 @@
+
+#Importar las librerías
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#se define la ruta del archivo
-df_movies = pd.read_csv("Data/Rotten Tomatoes Movies.csv")
-print(df_movies.head())
+#Se define la ruta del archivo
+df_movies= pd.read_csv("./Data/Rotten Tomatoes Movies.csv")
 
-# 1. Crear una copia del DataFrame original
-df_movies_copy = df_movies.copy()
+#Se crea una copia del DataFrame original
+df_movies_copy=df_movies.copy()
 
-# 2. Dividir la columna 'genre' por comas y explotar la lista
+#separar los géneros
+
 df_genres_exploded = df_movies_copy.assign(
-    genre=df_movies_copy['genre'].str.split(',')
+  genre=df_movies_copy['genre'].str.split(',')
 ).explode('genre')
 
-# 3. Limpiar espacios en blanco en los géneros
+print(df_movies_copy)
 df_genres_exploded['genre'] = df_genres_exploded['genre'].str.strip()
 
-# 4. Calcular el promedio de audiencia por género
-genre_avg_rating = df_genres_exploded.groupby('genre')['audience_rating'].mean()
+#Calcular el promedio de audience_rating para cada género individual
+promedio_audiencia_géneros= df_genres_exploded.groupby('genre')['audience_rating'].mean().sort_values(ascending=False)
+print(promedio_audiencia_géneros)
 
-# 5. Obtener los 10 géneros con mejor promedio de calificación
-top_10_genres = genre_avg_rating.sort_values(ascending=False).head(10)
+# •	Muestra los 10 géneros con el promedio de calificación de audiencia más alto haciendo uso de un diagrama de pastel.
+Top_10_generos= promedio_audiencia_géneros.head(10)
+print(Top_10_generos)
 
-# 6. Crear gráfico de pastel
-plt.figure(figsize=(8, 6))
-plt.pie(top_10_genres, labels=top_10_genres.index, startangle=90)
-plt.title('Top 10 películas con mejor promedio de valoración')
-plt.axis('equal')  # Mantener forma circular
+#Propiedades del gráfico
+plt.figure(figsize= (10,10))
+plt.pie(Top_10_generos,labels=Top_10_generos.index, startangle=140)
+plt.title("Top 10 géneros con mejor promedio de valoración")
 plt.tight_layout()
 plt.show()
